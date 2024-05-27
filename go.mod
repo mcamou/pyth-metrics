07f42368e4cca12a795f6d9d3b1607e0101fb04e
mcamou <mcamou@users.noreply.github.com>
2024-05-27 18:46:27 +0200

    Refactor and add Prometheus endpoint

--- go.mod
+++ go.mod
@@ -5,8 +5,18 @@ go 1.22.2
 require github.com/blocto/solana-go-sdk v1.30.0
 
 require (
+	github.com/beorn7/perks v1.0.1 // indirect
+	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/prometheus/client_model v0.5.0 // indirect
+	github.com/prometheus/common v0.48.0 // indirect
+	github.com/prometheus/procfs v0.12.0 // indirect
+	golang.org/x/sys v0.17.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
+
+require (
 	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
 	github.com/cbrewster/slog-env v0.1.1
 	github.com/mr-tron/base58 v1.2.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/prometheus/client_golang v1.19.1
 )
--- main.go
+++ main.go
@@ -15,6 +15,77 @@ import (
 	"github.com/blocto/solana-go-sdk/client"
 	"github.com/blocto/solana-go-sdk/rpc"
 	slogenv "github.com/cbrewster/slog-env"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type metric struct {
+	totalSlots     uint64
+	blocksProduced uint64
+}
+
+type clusterMetrics = map[string]metric
+
+type influxConfig struct {
+	influxUrl    string
+	influxOrg    string
+	influxUser   string
+	influxPass   string
+	influxBucket string
+	writeUrl     string
+}
+
+func influxConfigFromEnv() influxConfig {
+	url := os.Getenv("INFLUX_URL")
+	org := os.Getenv("INFLUX_ORG")
+	bucket := os.Getenv("INFLUX_BUCKET")
+
+	conf := influxConfig{
+		influxUrl:    url,
+		influxOrg:    org,
+		influxBucket: bucket,
+		influxUser:   os.Getenv("INFLUX_USER"),
+		influxPass:   os.Getenv("INFLUX_PASSWORD"),
+		writeUrl:     fmt.Sprintf("%s/write?org=%s&db=%s", url, org, bucket),
+	}
+
+	return conf
+}
+
+func (conf influxConfig) enabled() bool {
+	return conf.influxUrl != "" &&
+		conf.influxOrg != "" &&
+		conf.influxUser != "" &&
+		conf.influxPass != "" &&
+		conf.influxBucket != ""
+}
+
+var (
+	blocksProduced = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "pyth",
+			Name:      "blocks_produced",
+			Help:      "Number of blocks produced by a node in a specific epoch",
+		},
+		[]string{
+			"node_id",
+			"epoch",
+		},
+	)
+
+	skippedSlots = promauto.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Namespace: "pyth",
+			Name:      "skipped_slots",
+			Help:      "Number of slots skipped by a node in a specific epoch",
+		},
+		[]string{
+			"node_id",
+			"epoch",
+		},
+	)
 )
 
 func exitIfError(err error, log *slog.Logger) {
@@ -22,6 +93,14 @@ func exitIfError(err error, log *slog.Logger) {
 		log.Error("Fatal error", "error", err)
 		os.Exit(1)
 	}
+}
+
+func getEnv(varName string, defaultValue string) string {
+	value := os.Getenv(varName)
+	if value == "" {
+		return defaultValue
+	}
+	return value
 }
 
 func epochSlots(c *client.Client, log *slog.Logger, epoch uint64) (uint64, uint64, error) {
@@ -51,24 +130,7 @@ func epochSlots(c *client.Client, log *slog.Logger, epoch uint64) (uint64, uint6
 	return firstSlot, lastSlot, nil
 }
 
-func getMetrics(blockProduction map[string][]uint64, epoch uint64) string {
-	metrics := strings.Builder{}
-	for id, slots := range blockProduction {
-		metrics.WriteString(
-			fmt.Sprintf(
-				"leader_stats,host_id=%s,epoch=%d skipped_slots=%d,blocks_produced=%d\n",
-				id,
-				epoch,
-				slots[0]-slots[1],
-				slots[1],
-			),
-		)
-	}
-
-	return metrics.String()
-}
-
-func getBlockProductionForEpoch(c *client.Client, log *slog.Logger, epoch uint64) (map[string][]uint64, error) {
+func getBlockProductionForEpoch(c *client.Client, log *slog.Logger, epoch uint64) (clusterMetrics, error) {
 	firstSlot, lastSlot, err := epochSlots(c, log, epoch)
 	if err != nil {
 		return nil, err
@@ -90,24 +152,139 @@ func getBlockProductionForEpoch(c *client.Client, log *slog.Logger, epoch uint64
 	}
 	log.Debug("Got BlockProduction", "value", blockProductionResponse)
 
-	return blockProductionResponse.Result.Value.ByIdentity, nil
+	metrics := make(clusterMetrics)
+	for id, slots := range blockProductionResponse.Result.Value.ByIdentity {
+		metrics[id] = metric{
+			totalSlots:     slots[0],
+			blocksProduced: slots[1],
+		}
+	}
+
+	return metrics, nil
 }
 
-func getEnv(varName string, defaultValue string, log *slog.Logger) string {
-	value := os.Getenv(varName)
-	if value == "" {
-		return defaultValue
+func getMetricsForEpoch(c *client.Client, epoch uint64, log *slog.Logger) (clusterMetrics, uint64, error) {
+	metrics, err := getBlockProductionForEpoch(c, log, epoch)
+	if err != nil {
+		log.Error("Error calling getBlockProduction", "error", err, "epoch", epoch)
+		return clusterMetrics{}, 0, err
 	}
-	return value
+
+	log.Debug("Calling GetEpochInfo")
+	epochInfo, err := c.GetEpochInfo(context.TODO())
+	if err != nil {
+		log.Error("Error calling GetEpochInfo", "error", err)
+		return clusterMetrics{}, 0, err
+	}
+
+	return metrics, epochInfo.Epoch, nil
+}
+
+func updatePrometheusMetrics(metrics clusterMetrics, epoch uint64) {
+	epochStr := fmt.Sprintf("%d", epoch)
+
+	for id, m := range metrics {
+		blocksProduced.WithLabelValues(id, epochStr).Set(float64(m.blocksProduced))
+		skippedSlots.WithLabelValues(id, epochStr).Set(float64(m.totalSlots - m.blocksProduced))
+	}
+}
+
+func formatMetricsForInflux(metrics clusterMetrics, epoch uint64) string {
+	ret := strings.Builder{}
+	for id, m := range metrics {
+		ret.WriteString(
+			fmt.Sprintf(
+				"leader_stats,host_id=%s,epoch=%d skipped_slots=%d,blocks_produced=%d\n",
+				id,
+				epoch,
+				m.totalSlots-m.blocksProduced,
+				m.blocksProduced,
+			),
+		)
+	}
+
+	return ret.String()
+}
+
+func sendToInflux(metrics clusterMetrics, epoch uint64, conf influxConfig, log *slog.Logger) {
+	m := formatMetricsForInflux(metrics, epoch)
+
+	h := &http.Client{}
+
+	rdr := strings.NewReader(m)
+	req, err := http.NewRequest("POST", conf.writeUrl, rdr)
+	if err != nil {
+		log.Error("Error creating InfluxDB POST request", "error", err, "writeUrl", conf.writeUrl, "payload", metrics)
+		return
+	}
+	req.SetBasicAuth(conf.influxUser, conf.influxPass)
+
+	log.Debug("POSTing to InfluxDB")
+	resp, err := h.Do(req)
+	if err != nil {
+		log.Error("Error POSTing to InfluxDB", "error", err, "writeUrl", conf.writeUrl, "user", conf.influxUser, "payload", metrics)
+		return
+	}
+
+	// If there was an error when posting the data, do not move to the next epoch
+	if resp.StatusCode >= 400 {
+		body, err := io.ReadAll(resp.Body)
+		exitIfError(err, log)
+
+		log.Error("Error while talking to InfluxDB", "status", resp.StatusCode, "body", body)
+		return
+	}
+}
+
+func verboseMetrics(c *client.Client, epoch uint64, metrics clusterMetrics, log *slog.Logger) {
+	startSlot, endSlot, err := epochSlots(c, log, epoch)
+	exitIfError(err, log)
+	startBlock, err := c.GetBlock(context.TODO(), startSlot)
+	startHeight := "Unknown"
+	startTime := "Unknown"
+	if err != nil {
+		log.Warn("Error while getting start slot", "Error", err)
+	} else {
+		startHeight = fmt.Sprintf("%d", *startBlock.BlockHeight)
+		startTime = fmt.Sprintf("%v", *startBlock.BlockTime)
+	}
+	endBlock, err := c.GetBlock(context.TODO(), endSlot)
+	exitIfError(err, log)
+
+	m := formatMetricsForInflux(metrics, epoch)
+
+	fmt.Printf(
+		"Current epoch: %d\nStart slot: %v (%v)\nCurrent slot: %v (%v)\n\nMetrics:\n%s\n",
+		epoch,
+		startHeight,
+		startTime,
+		*endBlock.BlockHeight,
+		*endBlock.BlockTime,
+		m,
+	)
+}
+
+func updateMetrics(c *client.Client, epoch uint64, verbose bool, influxConfig influxConfig, log *slog.Logger) (uint64, error) {
+	metrics, newEpoch, err := getMetricsForEpoch(c, epoch, log)
+	if err != nil {
+		return 0, err
+	}
+
+	updatePrometheusMetrics(metrics, epoch)
+	if verbose {
+		verboseMetrics(c, epoch, metrics, log)
+	} else if influxConfig.enabled() {
+		sendToInflux(metrics, epoch, influxConfig, log)
+	}
+	return newEpoch, nil
 }
 
 func main() {
 	log := slog.New(slogenv.NewHandler(slog.NewJSONHandler(os.Stderr, nil)))
 
-	dryRun := false
-
 	help := flag.Bool("help", false, "Show help text")
 	helpShort := flag.Bool("h", false, "Show help text")
+	verbose := flag.Bool("verbose", false, "Run in verbose mode")
 	flag.Parse()
 
 	if *help || *helpShort {
@@ -115,119 +292,60 @@ func main() {
 COMMAND-LINE FLAGS
 
 --help - Show the help text
+--verbose - Display the InfluxDB metrics on stdout
 
 ENVIRONMENT VARIABLES
 
 RPC_URL - URL of the PythNet RPC host (optional, default is the PythNet RPC host)
-
-If any of the following are not specified, the program will run in dry-run mode, outputting the metrics to stdout:
+HTTP_PORT - HTTP port for Prometheus metrics (optional, default is 8000)
 
 INFLUX_URL - InfluxDB URL
 INFLUX_ORG - InfluxDB organization
 INFLUX_USER - InfluxDB user (for token v1 authentication)
 INFLUX_PASSWORD - InfluxDB password (for token v1 authentication)
-INFLUX_BUCKET - Bucket to save the metrics to
-`)
+INFLUX_BUCKET - Bucket to save the metrics to`)
 		os.Exit(0)
 	}
 
-	influxUrl := getEnv("INFLUX_URL", "", log)
-	influxOrg := getEnv("INFLUX_ORG", "", log)
-	influxUser := getEnv("INFLUX_USER", "", log)
-	influxPass := getEnv("INFLUX_PASSWORD", "", log)
-	influxBucket := getEnv("INFLUX_BUCKET", "", log)
+	influxConfig := influxConfigFromEnv()
 
-	if influxUrl == "" || influxOrg == "" || influxUser == "" || influxPass == "" || influxBucket == "" {
-		dryRun = true
-	}
+	rpcUrl := getEnv("RPC_URL", "https://api2.pythnet.pyth.network")
 
-	if dryRun {
-		log.Info("InfluxDB environment variables not set. Running in dry-run mode.")
-	}
-
-	rpcUrl := getEnv("RPC_URL", "https://api2.pythnet.pyth.network", log)
-
-	pollStr := getEnv("POLL_SECONDS", "30", log)
+	pollStr := getEnv("POLL_SECONDS", "30")
 	pollSecs, err := strconv.Atoi(pollStr)
 	exitIfError(err, log)
 
-	c := client.NewClient(rpcUrl)
-	h := &http.Client{}
+	httpStr := getEnv("HTTP_PORT", "8000")
+	httpPort, err := strconv.Atoi(httpStr)
+	exitIfError(err, log)
 
-	// TODO We should persist the currentEpoch so that we continue correctly on restart instead of always writing one epoch
-	currentEpoch := uint64(0)
-	writeUrl := fmt.Sprintf("%s/write?org=%s&db=%s", influxUrl, influxOrg, influxBucket)
+	log.Info("Serving Prometheus metrics", "port", httpPort, "URI", "/metrics")
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		http.ListenAndServe(fmt.Sprintf(":%d", httpPort), nil)
+	}()
+
+	c := client.NewClient(rpcUrl)
+
+	epochInfo, err := c.GetEpochInfo(context.TODO())
+	exitIfError(err, log)
+	currentEpoch := epochInfo.Epoch
 
 	ticker := time.NewTicker(time.Duration(pollSecs) * time.Second)
 
+	// TODO Persist the currentEpoch and publish the metrics for the intervening epochs
 	for ; ; <-ticker.C {
 		log.Debug("Starting loop")
 
-		if currentEpoch == 0 {
-			log.Debug("Initializing currentEpoch")
-			epochInfo, err := c.GetEpochInfo(context.TODO())
-			exitIfError(err, log)
-			currentEpoch = epochInfo.Epoch
-			log.Debug("currentEpoch initialized", "currentEpoch", currentEpoch)
-		}
-
-		blockProduction, err := getBlockProductionForEpoch(c, log, currentEpoch)
+		newEpoch, err := updateMetrics(c, currentEpoch, *verbose, influxConfig, log)
 		if err != nil {
-			log.Error("Error calling getBlockProduction", "error", err, "epoch", currentEpoch)
 			continue
 		}
 
-		metrics := getMetrics(blockProduction, currentEpoch)
-
-		log.Debug("Calling GetEpochInfo")
-		epochInfo, err := c.GetEpochInfo(context.TODO())
-		if err != nil {
-			log.Error("Error calling GetEpochInfo", "error", err)
-			continue
+		if currentEpoch != newEpoch {
+			log.Debug("updating currentEpoch", "currentEpoch", currentEpoch)
+			currentEpoch = newEpoch
+			updateMetrics(c, currentEpoch, *verbose, influxConfig, log)
 		}
-
-		if epochInfo.Epoch != currentEpoch {
-			log.Info("Switching epoch", "prev_epoch", currentEpoch, "new_epoch", epochInfo.Epoch)
-
-			blockProduction, err := getBlockProductionForEpoch(c, log, currentEpoch)
-			if err != nil {
-				log.Error("Error calling getBlockProduction", "error", err, "epoch", currentEpoch)
-				continue
-			}
-
-			metrics += getMetrics(blockProduction, currentEpoch)
-		}
-
-		if dryRun {
-			fmt.Printf("Sending:\n%s\n\n", metrics)
-			continue
-		}
-
-		rdr := strings.NewReader(metrics)
-		req, err := http.NewRequest("POST", writeUrl, rdr)
-		if err != nil {
-			log.Error("Error creating InfluxDB POST request", "error", err, "writeUrl", writeUrl, "payload", metrics)
-			continue
-		}
-		req.SetBasicAuth(influxUser, influxPass)
-
-		log.Debug("POSTing to InfluxDB")
-		resp, err := h.Do(req)
-		if err != nil {
-			log.Error("Error POSTing to InfluxDB", "error", err, "writeUrl", writeUrl, "user", influxUser, "payload", metrics)
-			continue
-		}
-
-		// If there was an error when posting the data, do not move to the next epoch
-		if resp.StatusCode >= 400 {
-			body, err := io.ReadAll(resp.Body)
-			exitIfError(err, log)
-
-			log.Error("Error while talking to InfluxDB", "status", resp.StatusCode, "body", body)
-			continue
-		}
-
-		currentEpoch = epochInfo.Epoch
-		log.Debug("updating currentEpoch", "currentEpoch", currentEpoch)
 	}
 }
